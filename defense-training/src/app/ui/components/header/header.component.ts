50a0f04b137fdf70019c9affe2de7d53cf9aee08
Jean Perriault <jpweb.taf@gmail.com>
2018-10-16 17:09:55 +0200

    Header component updated with bootstrap navbar

--- defense-training/src/app/ui/components/header/header.component.ts
+++ defense-training/src/app/ui/components/header/header.component.ts
@@ -8,10 +8,14 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
   public title = 'App Hash!';
+  public navbarOpen = false;
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
 }
